6598067fb195e76f3ca1608daac7d76acc3bd218
AntoXa PRO <info@antoxa.pro>
2024-03-28 14:49:08 +0300

    fix FilterHandlerMeta

--- src/types/hook.ts
+++ src/types/hook.ts
@@ -1,5 +1,7 @@
-export type THookFilterInfo = {
-	event: string,
-	keys: string[]
+import type { PrimaryKey } from '@directus/api/dist/types'
+
+export type TFilterHandlerMeta = {
+	event: string | 'items.create' | 'items.update',
+	keys: PrimaryKey[]
 	collection: string
 }
