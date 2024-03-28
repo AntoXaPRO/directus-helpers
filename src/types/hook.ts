df317b438d92e85634a06a16ae4a1ecd373e0543
AntoXa PRO <info@antoxa.pro>
2024-03-28 15:02:24 +0300

    fix filterhandlermeta

--- src/types/hook.ts
+++ src/types/hook.ts
@@ -1,6 +1,6 @@
 import type { PrimaryKey } from '@directus/api/dist/types'
 
-export type TFilterHandlerMeta = {
+export type TFilterHandlerMeta = Record<string, any> & {
 	event: string | 'items.create' | 'items.update',
 	keys: PrimaryKey[]
 	collection: string
