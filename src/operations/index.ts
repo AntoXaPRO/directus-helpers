540c165e943efc6d3163d6635990716de2be446d
AntoXa PRO <info@antoxa.pro>
2024-04-17 11:42:34 +0300

    OperationContextData

--- src/operations/index.ts
+++ src/operations/index.ts
@@ -1,1 +1,11 @@
+import type { Accountability } from '@directus/types'
+import type { TTrigger } from './trigger'
+
+export type TOperationContextData = {
+	$trigger: TTrigger,
+	$last: TTrigger
+	$accountability: Accountability
+	$env: Record<string, string>
+}
+
 export * from './trigger'
--- src/operations/trigger.ts
+++ src/operations/trigger.ts
@@ -1,7 +1,21 @@
-export type TTreggerData<TName extends string = string> = {
-	$trigger: {
-		body: {
-			collection: TName
-		}
+export type TTrigger = {
+	path: string
+	query: any,
+	body: {
+		collection: string
+	},
+	method: 'GET' | 'POST',
+	headers: {
+		host: string
+		connection: string
+		origin: string
+		referer: string
+		cookie: string
+		accept: string
+		'accept-encoding': string
+		'accept-language': string
+		'user-agent': string
+		'content-type': string
+		'content-length': string
 	}
 }
